ec0af4920cf1f9575d31351c1529aff94403a92e
mihkel <jupatz@gmail.com>
2016-05-16 18:13:17 +0300

    .

--- viewprofile.php
+++ viewprofile.php
@@ -1,6 +1,6 @@
 <?php
 require "core/init.php";
-if (isset($_GET["user"]) && strlen($_GET["user"]) > 0) {
+if (isset($_GET["user"]) && mb_strlen($_GET["user"]) > 0) {
 	$name = urldecode($_GET["user"]);
 	// $user = DB::getInstance()->query("SELECT username FROM users WHERE username=?", array($name));
 	$user = DB::getInstance()->query("SELECT username, first_name, last_name, email, username, DATE_FORMAT(date_added, '%D %b %Y') AS join_date FROM users WHERE username=?", array($name));
